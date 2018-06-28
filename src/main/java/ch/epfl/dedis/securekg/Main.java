6a044a75b1ac13397433cd82849dcfb0c6ce883b
kc1212 <kc1212@users.noreply.github.com>
2018-06-28 12:50:03 +0200

    Initial commit

---
+++ src/main/java/ch/epfl/dedis/securekg/Main.java
@@ -0,0 +1,25 @@
+package ch.epfl.dedis.securekg;
+
+import ch.epfl.dedis.lib.eventlog.Event;
+import ch.epfl.dedis.lib.exception.CothorityException;
+import ch.epfl.dedis.lib.omniledger.InstanceId;
+import ch.epfl.dedis.lib.omniledger.OmniledgerRPC;
+import ch.epfl.dedis.lib.omniledger.contracts.EventLogInstance;
+import ch.epfl.dedis.lib.omniledger.darc.Signer;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            OmniledgerRPC ol = ServerConfig.getOmniledgerRPC();
+            Signer admin = ServerConfig.getSigner();
+            EventLogInstance el = new EventLogInstance(ol, ServerConfig.getEventlogId());
+            InstanceId key = el.log(new Event("hello", "goodbye"), Arrays.asList(admin));
+            Thread.sleep(2 * ol.getConfig().getBlockInterval().toMillis());
+            System.out.println("got event: " + el.get(key));
+        } catch (InterruptedException | CothorityException e) {
+            System.out.println("error: " + e);
+        }
+    }
+}
